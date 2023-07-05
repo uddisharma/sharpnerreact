ef8c5df2cf1d3d1dbffd32dc069c9c58d9322ef5
uddisharma <uddibhardwaj08@gmail.com>
2023-07-05 11:47:16 +0530

    update

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,44 +1,83 @@
 import { useState } from "react";
+import { Toaster, toast } from "react-hot-toast";
 
 export default function Input() {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState({
+    name: "",
+    age: "",
+  });
+  let extisingmember = JSON.parse(localStorage.getItem("member"));
+  let data = extisingmember ? extisingmember : [];
+
+  const { name, age } = value;
+  const handleClick = () => {
+    document.getElementById("btn").style.backgroundColor = "green";
+    if (name == "") {
+      return toast.error("Please enter a name");
+    }
+    if (age == "") {
+      return toast.error("Please enter age");
+    }
+    if (age > 30) {
+      return toast.error("Age must be smaller than 30");
+    }
+    data.push(value);
+    localStorage.setItem("member", JSON.stringify(data));
+    toast.success("Member successfully added !");
+    setValue({ ...value, name: "", age: "" });
+  };
+
   return (
-    <div className="w-[40%] m-auto mt-16">
-      <label
-        htmlFor="price"
-        className="block text-sm font-medium leading-6 text-gray-900"
-      >
-        Add Task Amount
-      </label>
-      <div className="relative mt-2 rounded-md shadow-sm">
-        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-          {/* <span className="text-gray-500 sm:text-sm">$</span> */}
-        </div>
-        <input
-          value={value}
-          onChange={(e) => {
-            setValue(e.target.value);
-          }}
-          type="text"
-          name="price"
-          id="price"
-          className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          placeholder=""
-        />
-        <br />
-        <button
-          onClick={() => {
-            document.getElementById("btn").style.backgroundColor = "green";
-          }}
-          disabled={value == ""}
-          style={{ backgroundColor: value == "" ? "red" : "blue" }}
-          type="button"
-          id="btn"
-          class="  text-white v bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <div className="w-[40%] m-auto mt-16">
+        <label
+          htmlFor="price"
+          className="block text-sm font-medium leading-6 text-gray-900"
         >
-          Default
-        </button>
-        {/* <div className="absolute inset-y-0 right-0 flex items-center">
+          Add Member
+        </label>
+        <div className="relative mt-2 rounded-md shadow-sm">
+          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+            {/* <span className="text-gray-500 sm:text-sm">$</span> */}
+          </div>
+          <input
+            value={name}
+            onChange={(e) => {
+              setValue({ ...value, name: e.target.value });
+            }}
+            type="text"
+            name="name"
+            id="name"
+            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            placeholder="Name"
+          />
+          <br />
+          <input
+            value={age}
+            onChange={(e) => {
+              setValue({ ...value, age: e.target.value });
+            }}
+            type="number"
+            name="age"
+            id="age"
+            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            placeholder="Age"
+          />
+          <br />
+          <button
+            onClick={() => {
+              handleClick();
+            }}
+            disabled={name == ""}
+            style={{ backgroundColor: name == "" ? "red" : "blue" }}
+            type="button"
+            id="btn"
+            class="  text-white v bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Add
+          </button>
+          {/* <div className="absolute inset-y-0 right-0 flex items-center">
           <label htmlFor="currency" className="sr-only">
             Currency
           </label>
@@ -52,7 +91,31 @@ export default function Input() {
             <option>EUR</option>
           </select>
         </div> */}
+        </div>
+        {data.length > 0 ? (
+          <table class="table-auto">
+            <thead>
+              <tr>
+                <th>#Id</th>
+                <th>Name</th>
+                <th>Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.length > 0 &&
+                data.map((e, i) => (
+                  <tr>
+                    <td>{i + 1}</td>
+                    <td>{e.name}</td>
+                    <td>{e.age}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        ) : (
+          "no member added till now please add a member"
+        )}
       </div>
-    </div>
+    </>
   );
 }
